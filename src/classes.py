950345f2e41ea706150423078fd419aa861a54da
Dio Luwandri <diovian.liu@gmail.com>
2025-03-01 17:25:13 +0800

    implement line draw method

--- src/classes.py
+++ src/classes.py
@@ -1,14 +1,36 @@
 from tkinter import Tk, BOTH, Canvas
 
 
+class Point:
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+
+
+class Line:
+    def __init__(self, point1: Point, point2: Point):
+        self.point1 = point1
+        self.point2 = point2
+
+    def draw(self, canvas: Canvas, fill_colour: str):
+        x1 = self.point1.x
+        y1 = self.point1.y
+        x2 = self.point2.x
+        y2 = self.point2.y
+        canvas.create_line(x1, y1, x2, y2, fill=fill_colour, width=2)
+
+
 class Window:
-    def __init__(self, width, height):
+    def __init__(self, width: int, height: int):
         self.__root = Tk()
         self.__root.title("Maze Solver")
         self.__canvas = Canvas(self.__root, width=width, height=height)
         self.__canvas.pack(fill=BOTH)
         self.__running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
+
+    def draw_line(self, line: Line, fill_colour: str):
+        line.draw(self.__canvas, fill_colour)
 
     def redraw(self):
         self.__root.update_idletasks()
--- src/main.py
+++ src/main.py
@@ -1,10 +1,38 @@
 from classes import (
-    Window
+    Window,
+    Point,
+    Line
 )
+import random
 
 
 def main():
-    win = Window(800, 600)
+    width = 800
+    height = 600
+    win = Window(width, height)
+    p1 = Point(random.randint(0, width),
+               random.randint(0, height))
+    p2 = Point(random.randint(0, width),
+               random.randint(0, height))
+    p3 = Point(random.randint(0, width),
+               random.randint(0, height))
+    p4 = Point(random.randint(0, width),
+               random.randint(0, height))
+    p5 = Point(random.randint(0, width),
+               random.randint(0, height))
+    p6 = Point(random.randint(0, width),
+               random.randint(0, height))
+    points = [p1, p2, p3, p4, p5, p6]
+    colours = ["black", "red", "blue", "green", "cyan"]
+    for i in range(10):
+        point1 = random.choice(points)
+        point2 = random.choice(points)
+        line = Line(point1, point2)
+        fill_colour = random.choice(colours)
+        win.draw_line(line, fill_colour)
+        print(
+            f"Drawing a {fill_colour} line between points {point1} and {point2}")
+
     win.wait_for_close()
 
 
