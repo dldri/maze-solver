8107d027add86104bf82ad66831e19fe1bb4b8f3
Dio Luwandri <diovian.liu@gmail.com>
2025-03-02 16:31:59 +0800

    implemented wall breaking

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ def main():
     width = 800
     height = 600
     win = Window(width, height)
-    maze = Maze(100, 100, 10, 10, 20, 20, win)
+    maze = Maze(100, 100, 10, 10, 20, 20, win, 1)
 
     win.wait_for_close()
 
--- src/maze.py
+++ src/maze.py
@@ -1,6 +1,7 @@
 from cells import Cell
 from graphics import Window
 import time
+import random
 
 
 class Maze:
@@ -12,7 +13,8 @@ class Maze:
         num_cols: int,
         cell_size_x: int,
         cell_size_y: int,
-        win: Window = None
+        win: Window = None,
+        seed: int = None
     ):
         self._x1 = x1
         self._y1 = y1
@@ -21,29 +23,32 @@ class Maze:
         self._cell_size_x = cell_size_x
         self._cell_size_y = cell_size_y
         self._win = win
+        if seed:
+            random.seed(seed)
         self._cells = []
 
         self._create_cells()
         self._break_entrance_and_exit()
+        self._break_walls_r(0, 0)
 
     def _create_cells(self):
-        for col in range(self._num_cols):
+        for row in range(self._num_rows):
             buffer_list = []
-            for row in range(self._num_rows):
+            for col in range(self._num_cols):
                 buffer_list.append(Cell(self._win))
             self._cells.append(buffer_list)
-        for i in range(self._num_cols):
-            for j in range(self._num_rows):
-                self._draw_cell(i, j)
+        for row in range(self._num_rows):
+            for col in range(self._num_cols):
+                self._draw_cell(row, col)
 
-    def _draw_cell(self, i: int, j: int):
+    def _draw_cell(self, row: int, col: int):
         if self._win is None:
             return
-        x1 = self._x1 + (i * self._cell_size_x)
-        y1 = self._y1 + (j * self._cell_size_y)
+        x1 = self._x1 + (col * self._cell_size_x)
+        y1 = self._y1 + (row * self._cell_size_y)
         x2 = x1 + self._cell_size_x
         y2 = y1 + self._cell_size_y
-        self._cells[i][j].draw(x1, y1, x2, y2)
+        self._cells[row][col].draw(x1, y1, x2, y2)
         self._animate()
 
     def _animate(self):
@@ -53,6 +58,48 @@ class Maze:
     def _break_entrance_and_exit(self):
         self._cells[0][0].has_top_wall = False
         self._draw_cell(0, 0)
-        self._cells[self._num_cols - 1][self._num_rows -
+        self._cells[self._num_rows - 1][self._num_cols -
                                         1].has_bottom_wall = False
-        self._draw_cell(self._num_cols - 1, self._num_rows - 1)
+        self._draw_cell(self._num_rows - 1, self._num_cols - 1)
+
+    def _break_walls_r(self, row: int, col: int):
+        self._cells[row][col].visited = True
+        moves = [
+            (0, -1),  # look left
+            (-1, 0),  # look up
+            (0, 1),  # look right
+            (1, 0)  # look down
+        ]
+        while True:
+            queue = []
+            for d_row, d_col in moves:
+                ni, nj = row + d_row, col + d_col
+                if (
+                    0 <= ni < self._num_rows
+                    and 0 <= nj < self._num_cols
+                    and not self._cells[ni][nj].visited
+                ):
+                    queue.append((ni, nj, d_row, d_col))
+            if not queue:
+                self._draw_cell(row, col)
+                return
+            selection = random.choice(queue)
+            ni, nj, d_row, d_col = selection
+
+            if d_col == -1:  # moving left
+                self._cells[row][col].has_left_wall = False
+                self._cells[ni][nj].has_right_wall = False
+
+            elif d_row == -1:  # moving up
+                self._cells[row][col].has_top_wall = False
+                self._cells[ni][nj].has_bottom_wall = False
+
+            elif d_col == 1:  # moving right
+                self._cells[row][col].has_right_wall = False
+                self._cells[ni][nj].has_left_wall = False
+
+            elif d_row == 1:  # moving down
+                self._cells[row][col].has_bottom_wall = False
+                self._cells[ni][nj].has_top_wall = False
+
+            self._break_walls_r(ni, nj)
